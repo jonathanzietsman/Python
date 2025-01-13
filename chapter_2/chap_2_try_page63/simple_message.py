f6ae2eb5af9f525eac455e6150385df793aacbbb
Jonathan <131307491+jonathanzietsman@users.noreply.github.com>
2025-01-13 18:26:37 +0200

    Add files via upload

---
+++ chapter_2/chap_2_try_page63/simple_message.py
@@ -0,0 +1,2 @@
+message = "My message..."
+print(message)
---
+++ chapter_2/chap_2_try_page71/famous_quote.py
@@ -0,0 +1,6 @@
+author = "Albert Einstein"
+quote = "A person who never made a mistake never tried anything new."
+
+message = (f'{author} once said, "{quote}"')
+
+print(message)
