bdf8a97a2e5325e602af65fef6e38665ba381f32
Jonathan <131307491+jonathanzietsman@users.noreply.github.com>
2025-01-20 19:20:20 +0200

    chap4_ex2

---
+++ chapter_4/chap_4_try_page110/summing_a_million.py
@@ -0,0 +1,11 @@
+numbers = list(range(1, 10000001))
+
+amin = min(numbers)
+amax = max(numbers)
+total = sum(numbers)
+
+print(f'Smallest Number: {amin}')
+
+print(f'\nLargest Number: {amax}')
+
+print(f'\nTotal Of All Numbers: {total}')
---
+++ chapter_4/chap_4_try_page110/threes.py
@@ -0,0 +1,4 @@
+numbers = list(range(3, 31, 3))
+
+for n in numbers:
+    print(n)
