ee52c0723593be6f58e25b5fdc74b341c4bc8681
Jonathan <131307491+jonathanzietsman@users.noreply.github.com>
2025-01-20 19:19:12 +0200

    chap4 ex1

---
+++ chapter_4/chap_4_try_page105/animals.py
@@ -0,0 +1,7 @@
+# Think of at least three different animals that have a common characteristic. Store the names of these animals in a list, and then use a for loop to print out the name of each animal.
+animals = ["Dog", "Cat", "Rabbit"]
+
+for animal in animals :
+    print(f'A {animal} would make a great pet.')
+
+print('Any of these animals would make great pets!')
---
+++ chapter_4/chap_4_try_page105/pizzas.py
@@ -0,0 +1,8 @@
+# Think of at least three kinds of your favorite pizza. Store these pizza names in a list, and then use a for loop to print the name of each pizza.
+pizza = ["Pepperoni", "Margherita", "BBQ Chicken"]
+
+for i in pizza:
+    # Modify your for loop to print a sentence using the name of the pizza instead of printing just the name of the pizza. For each pizza you should have one line of output containing a simple statement like I like pepperoni pizza.
+    print(f'I love {i} pizza!')
+
+print('\nI really love pizza!')
