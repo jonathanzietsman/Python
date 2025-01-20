0c605868fd7ae5e37c2562e5c818e2758851c564
Jonathan <131307491+jonathanzietsman@users.noreply.github.com>
2025-01-20 19:21:15 +0200

    ch4 ex4

---
+++ chapter_4/chap_4_try_page119/buffet.py
@@ -0,0 +1,11 @@
+foods = ('Pizza', 'Fried Chicken', 'Burger', 'Salad', 'Sandwich')
+
+for food in foods:
+    print(food)
+    
+print()
+""" foods[1] = ('Chips') """
+
+foods = ('Pizza', 'Salad', 'Sandwich', 'Chips', 'Fries')
+for food in foods:
+    print(food)
