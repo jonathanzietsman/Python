fbf305fe5a7536dbf55fd9036db5ff513afca987
Jonathan <131307491+jonathanzietsman@users.noreply.github.com>
2025-01-28 17:38:15 +0200

    chap 2_pg19

---
+++ chapter_2/chap_2_try_page19/simple_messages.py
@@ -0,0 +1,8 @@
+mes = "Unchanged Message"
+print(mes)
+
+mes = "Changed Message"
+print(mes)
+
+""" Assign a message to a variable, and print that message. Then change
+the value of the variable to a new message, and print the new message """
